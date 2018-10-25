51d4f9aa6a43d2f80c8dbb7531e440e117f19182
Vedant Agarwala <mail@vedant-agarwala.com>
2018-10-25 17:11:37 +0530

    parse strings

--- lib/parser.rb
+++ lib/parser.rb
@@ -9,17 +9,20 @@ class Parser
 
   def initialize(input)
     @unparsed_string = input
-    @parsed_object = nil
-    parse
+    @parsed_object = parse
   end
 
 private
   def parse
     first_char = unparsed_string[0]
 
-    @parsed_object =
-    if first_char.numeric?
-      parse_int
+    case first_char
+    when '"'
+      parse_string
+    else
+      if first_char.numeric?
+        parse_int
+      end
     end
   end
 
@@ -27,5 +30,13 @@ private
     int = @unparsed_string.to_i
     @unparsed_string.sub! int.to_s, ''
     int
+  end
+
+  def parse_string
+    @unparsed_string.slice! 0 # remove the first "
+    last_quotes_index = @unparsed_string.index '"'
+    string = @unparsed_string[0...last_quotes_index]
+    @unparsed_string = @unparsed_string[(last_quotes_index + 1)..-1]
+    string
   end
 end
