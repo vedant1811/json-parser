eaa98f3cea7044205265e143b6879c53b22ad862
Vedant Agarwala <mail@vedant-agarwala.com>
2018-10-25 15:34:44 +0530

    Parse ints

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,13 +1,31 @@
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
 class Parser
   attr_reader :unparsed_string, :parsed_object
 
   def initialize(input)
     @unparsed_string = input
     @parsed_object = nil
+    parse
   end
 
 private
   def parse
+    first_char = unparsed_string[0]
 
+    @parsed_object =
+    if first_char.numeric?
+      parse_int
+    end
+  end
+
+  def parse_int
+    int = @unparsed_string.to_i
+    @unparsed_string.sub! int.to_s, ''
+    int
   end
 end
